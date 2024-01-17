9939f63e641366a09ddf8ab481b9cd89a2628977
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-17 18:45:20 -0400

    Added Quick Sort function

---
+++ 3-quick_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - swaps two integers
+ * @a: first integer
+ * @b: second integer
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of array
+*/
+void quick_sort(int *array, size_t size)
+{
+	int pivot = array[size - 1];
+	size_t i = 0, j = 0;
+
+	if (size < 2)
+		return;
+
+	for (; j < size - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != j)
+	{
+		swap(&array[i], &array[j]);
+		print_array(array, size);
+	}
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
+}
