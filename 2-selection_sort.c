b2155dbfeb468946dc8250fbf280830e6be4625b
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-17 17:06:04 -0400

    fix for selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,17 +1,33 @@
 #include "sort.h"
 #include <stdio.h>
+
+/**
+ * swap - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 /**
  * selection_sort - sorts an array of integers in ascending order
  * @array: array to be sorted
  * @size: size of array
-*/
+ */
+
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, k, min_pos;
+	int temp;
+	size_t j, i, min_pos;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_pos = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_pos])
@@ -19,11 +35,14 @@ void selection_sort(int *array, size_t size)
 				min_pos = j;
 			}
 		}
+
 		if (min_pos != i)
 		{
-			k = array[i];
+			temp = array[i];
 			array[i] = array[min_pos];
-			array[min_pos] = k;
+			array[min_pos] = temp;
+
+			print_array(array, size);
 		}
 	}
 }
